3ed4cb6b37f3fd92809b4eb0d6afdcfee8a90162
9455 <praveenku9452@gmail.com>
2023-05-18 12:06:22 +0530

    Education and Training pages added

--- Nationalyouthprogramme/urls.py
+++ Nationalyouthprogramme/urls.py
@@ -18,4 +18,15 @@ urlpatterns = [
     path('sector',sector,name='sector'),
     path('contact_us',contact_us,name='contact_us'),
     path('news',news,name='news'),
+
+
+    path('course_and_college',course_and_college,name='course_and_college'),
+    path('admission_registration',admission_registration,name='admission_registration'),
+    path('exam_registration',exam_registration,name='exam_registration'),
+    path('mark_registration',mark_registration,name='mark_registration'),
+    path('add_on_programme',add_on_programme,name='add_on_programme'),
+    path('application_for_affiliation',application_for_affiliation,name='application_for_affiliation'),
+    path('pay_online_fees',pay_online_fees,name='pay_online_fees'),
+    path('e_office_file_sanction',e_office_file_sanction,name='e_office_file_sanction'),
+    path('student_marks',student_marks,name='student_marks'),
 ]
